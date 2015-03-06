ee0294dd505e2517eea368c579e52a52b78b32a1
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-03-07 11:41:16 +1300

    Updated dumps.

---
+++ dumps/3.sql
@@ -0,0 +1,82 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Mar 06, 2015 at 10:38 PM
+-- Server version: 5.5.41-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `salo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `adverts`
+--
+
+CREATE TABLE IF NOT EXISTS `adverts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(30) CHARACTER SET utf16 COLLATE utf16_bin NOT NULL,
+  `text` text CHARACTER SET utf16 COLLATE utf16_bin NOT NULL,
+  `startDate` date NOT NULL,
+  `endDate` date NOT NULL,
+  `category` int(5) NOT NULL,
+  `image` varchar(64) CHARACTER SET utf16 COLLATE utf16_bin DEFAULT NULL,
+  `email` varchar(50) CHARACTER SET utf16 COLLATE utf16_bin DEFAULT NULL,
+  `phone` varchar(17) CHARACTER SET utf16 COLLATE utf16_bin DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `CategoryId` (`category`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `adverts`
+--
+
+INSERT INTO `adverts` (`id`, `title`, `text`, `startDate`, `endDate`, `category`, `image`, `email`, `phone`) VALUES
+(2, 'ХУЙНЯ', 'ХУЙНЯ', '2015-03-01', '2015-03-02', 0, 'ХУЙНЯ', 'ХУЙНЯ', 'ХУЙНЯ');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(25) NOT NULL,
+  `password` varchar(70) NOT NULL,
+  `role` varchar(100) NOT NULL,
+  `name` text NOT NULL,
+  `dob` date NOT NULL,
+  `email` varchar(70) NOT NULL,
+  `number` varchar(17) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
