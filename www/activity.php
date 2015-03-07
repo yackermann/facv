68fda43411e67e69c20370d45edcad82cac71849
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-03-07 17:45:46 +1300

    Styling and merged conflicts

--- www/activity.php
+++ www/activity.php
@@ -1,40 +1,40 @@
 <?php
-require_once('includes/connect.php');
-// this gets the theme that was passed in the URL and places it in $link
-if (isset($_GET['theme'])) {
-	$link = $_GET['theme'];
-} else {
-	$link='relax';
-}
+	require_once('includes/connect.php');
+	// this gets the theme that was passed in the URL and places it in $link
+	if (isset($_GET['theme'])) {
+		$link = $_GET['theme'];
+	} else {
+		$link='relax';
+	}
 
-//this helps to prevent sql injection
-$link=htmlspecialchars($link,ENT_QUOTES, 'UTF-8');
-//prepare the navigation result set
-$navigation_sql = "SELECT * FROM activities WHERE theme LIKE :theme";
-$resultNavigation = $pdo -> prepare($navigation_sql);
-//bind the query
-$resultNavigation->bindValue(':theme','%'.$link.'%',PDO::PARAM_STR);
-//execute the query
-$resultNavigation -> execute();
-/* check to see if the URL contains the id if not give it a default value */
-if (isset($_GET['id'])) {
-	$id = $_GET['id'];
-} else {
-	$id=4;
-}
-$id=htmlspecialchars($id,ENT_QUOTES, 'UTF-8');
-// get the activity recordset
-//$activity_sql="SELECT * from activities WHERE id=:id";
-$activity_sql= "SELECT *
-FROM activities
-Inner Join tourguide on tourguide.id = activities.tourguide_id
-WHERE activities.id =:id
-";
-$resultActivity = $pdo -> prepare($activity_sql);
-//bind the query
-$resultActivity->bindValue(':id',$id,PDO::PARAM_STR);
-//execute the query
-$resultActivity -> execute();
+	//this helps to prevent sql injection
+	$link = htmlspecialchars($link, ENT_QUOTES, 'UTF-8');
+	//prepare the navigation result set
+	$navigation_sql = "SELECT * FROM activities WHERE theme LIKE :theme";
+	$resultNavigation = $pdo -> prepare($navigation_sql);
+	//bind the query
+	$resultNavigation->bindValue(':theme','%'.$link.'%',PDO::PARAM_STR);
+	//execute the query
+	$resultNavigation -> execute();
+	/* check to see if the URL contains the id if not give it a default value */
+	if (isset($_GET['id'])) {
+		$id = $_GET['id'];
+	} else {
+		$id=4;
+	}
+	$id=htmlspecialchars($id,ENT_QUOTES, 'UTF-8');
+	// get the activity recordset
+	//$activity_sql="SELECT * from activities WHERE id=:id";
+	$activity_sql= "SELECT *
+	FROM activities
+	Inner Join tourguide on tourguide.id = activities.tourguide_id
+	WHERE activities.id =:id
+	";
+	$resultActivity = $pdo -> prepare($activity_sql);
+	//bind the query
+	$resultActivity->bindValue(':id',$id,PDO::PARAM_STR);
+	//execute the query
+	$resultActivity -> execute();
 ?>
 <!DOCTYPE html>
 <html lang="en">
