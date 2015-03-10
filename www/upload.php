4f970d29439f747d243fcbf778df3c8fd6793af7
Fulton <Sharleyv@meta.ua>
2015-03-10 21:04:34 +0200

    Updated upload.php
    Added checking formats.
    
    p.s. Я знаю что эти коммиты по 1 строчке почти. Просто потом
    додумываюсь. Сейчас ищу как переформатировать в жпг.

--- www/upload.php
+++ www/upload.php
@@ -1,8 +1,19 @@
 <?php
    if(is_uploaded_file($_FILES["filename"]["tmp_name"]))
    {
-	$_FILES["filename"]["name"] = md5($_FILES["filename"]["name"]);
-     	move_uploaded_file($_FILES["filename"]["tmp_name"], ".\\uploads".$_FILES["filename"]["name"]);
+	if($_FILES['filename']['type'] == "image/gif" 
+		|| $_FILES['filename']['type'] == "image/jpg" 
+		|| $_FILES['filename']['type'] == "image/jpeg" 
+		|| $_FILES['filename']['type'] == "image/png" 
+		|| $_FILES['filename']['type'] == "image/bmp" ) //need to check, maybe i missed some formats
+	{
+		$_FILES["filename"]["name"] = md5($_FILES["filename"]["name"]);
+     		move_uploaded_file($_FILES["filename"]["tmp_name"], ".\\uploads".$_FILES["filename"]["name"]);
+	}
+	else
+	{
+		echo("Sorry but we need image");
+	}
    } 
    else 
    {
