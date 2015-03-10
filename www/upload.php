44bb25c393c18b2c13e9f8c5f700cfa92430ba39
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-03-11 10:36:07 +1300

    Improved readability and styling.

--- www/upload.php
+++ www/upload.php
@@ -1,31 +1,17 @@
 <?php
-<<<<<<< Updated upstream
-   if(is_uploaded_file($_FILES["filename"]["tmp_name"]))
-   {
-	if($_FILES['filename']['type'] == "image/gif" 
-		|| $_FILES['filename']['type'] == "image/jpg" 
-		|| $_FILES['filename']['type'] == "image/jpeg" 
-		|| $_FILES['filename']['type'] == "image/png" 
-		|| $_FILES['filename']['type'] == "image/bmp" ) //need to check, maybe i missed some formats
-	{
-		$_FILES["filename"]["name"] = md5($_FILES["filename"]["name"]);
-     		move_uploaded_file($_FILES["filename"]["tmp_name"], ".\\uploads".$_FILES["filename"]["name"].'.jpg');
-	}
-	else
-	{
-		echo("Sorry but we need image");
-	}
-   } 
-   else 
-   {
-      echo("Error");
-   }
-=======
+	$allowed_types = array("image/gif", "image/jpg", "image/jpeg", "image/png", "image/bmp");
+
 	if(is_uploaded_file($_FILES["filename"]["tmp_name"])){
-		$_FILES["filename"]["name"] = "1";
-	  	move_uploaded_file($_FILES["filename"]["tmp_name"], ".\\uploads".$_FILES["filename"]["name"]);
+
+		if(in_array($_FILES['filename']['type'], $allowed_types)){ //need to check, maybe i missed some formats
+
+			$_FILES["filename"]["name"] = md5($_FILES["filename"]["name"]);
+		  	move_uploaded_file($_FILES["filename"]["tmp_name"], ".\\uploads".$_FILES["filename"]["name"].'.jpg');
+	
+		}else{
+			echo("Sorry but we need image");
+		}
 	}else{
 		echo("Error");
 	}
->>>>>>> Stashed changes
 ?>
