7958ecf58f41bec9a5cc9856afd91504545acc29
Fulton <Sharleyv@meta.ua>
2015-03-10 20:28:43 +0200

    Added md5 hashing for name of the image

--- www/upload.php
+++ www/upload.php
@@ -1,7 +1,7 @@
 <?php
    if(is_uploaded_file($_FILES["filename"]["tmp_name"]))
    {
-	$_FILES["filename"]["name"] = "1";
+	$_FILES["filename"]["name"] = md5($_FILES["filename"]["name"]);
      	move_uploaded_file($_FILES["filename"]["tmp_name"], ".\\uploads".$_FILES["filename"]["name"]);
    } 
    else 
