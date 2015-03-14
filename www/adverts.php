c707c72c741cccf4a2de9abd9a3e78336e68e559
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-03-14 17:54:18 +1300

    Rewritten SQL requests to separate class. Learned PHP classes. Decreased hate to PHP. Made code more expandable and more elegant. Created SQLRequests class.  + Created make sqlmethod for pdo sql reuest.  + Created getCategories and getAdverts methods for getting adverts and categories.  + Added debug code.(Commented) for future use.

--- www/adverts.php
+++ www/adverts.php
@@ -1,57 +1,27 @@
 <?php
 
 	
+	//UNCOMMENT FOR DEBUG
+	// error_reporting(E_ALL); 
+	// ini_set( 'display_errors','1');
 
-	//include database connection
+	//Set header to JSON
 	header('Content-Type: application/json');
 
-	include 'includes/connect.php';
+	//Add SQL methods
 	include 'includes/sql_requests.php';
 
-	$stmt = $pdo->prepare( $sqlr["adverts"] );
-	$stmt->execute();
+	//Initialize new SQLRequests object
+	$SQLReq = new SQLRequests();
 
-	//Get number of rows
-	$num = $stmt->rowCount();
-
-
+	//Getting info
 	$content = array(
 		'status' 	 =>  '',
-		'categories' =>  array(),
-		'adverts' 	 =>  array()
+		'categories' =>  $SQLReq -> getCategories(),
+		'adverts' 	 =>  $SQLReq -> getAdverts()
 	);
-	if($num>0){ //check if more than 0 record found
-		
-		//retrieve our table contents
-		//fetch() is faster than fetchAll()
-		//http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
-		while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
 
-			//extract row
-			//this will make $row['firstname'] to
-			//just $firstname only
-			extract($row);
-
-			$item = array(
-				'id' 		=> $id,
-				'title' 	=> $title,
-				'text' 		=> $text,
-				'startDate' => $startDate,
-				'endDate' 	=> $endDate,
-				'category' 	=> $category,
-				'image' 	=> $image,
-				'email' 	=> $email,
-				'phone' 	=> $phone
-			);
-			array_push($content['adverts'], $item);
-
-		}
-		$content['status'] = 200;
-		echo json_encode($content);
-
-	}else{ //if no records found
-		$content['status'] = 404;
-		echo json_encode();
-	}
+	//Returning JSON
+	echo json_encode($content);
 	
 ?>
--- www/includes/sql_requests.php
+++ www/includes/sql_requests.php
@@ -1,7 +1,58 @@
 <?php
-	$sqlr = [
-		"adverts" => 'SELECT id, title, text, startDate, endDate, category, image, email, phone FROM adverts',
-		"categories" => 'SELECT id, name FROM categories',
-		"users" => 'SELECT id, login, password, role, name, dob, email, number FROM users',
-	];
+	include 'includes/connect.php';
+	class SQLRequests{
+		private $sqlr = array(
+			'adverts' => 'SELECT id, title, text, startDate, endDate, category, image, email, phone FROM adverts',
+			'categories' => 'SELECT id, loc_ru, loc_ua, loc_en FROM categories',
+			'users' => 'SELECT id, login, password, role, name, dob, email, number FROM users',
+		);
+
+		public function test(){
+			echo var_dump();
+		}
+		private function makeSQL($request=''){
+			if($request !== ''){
+
+				//Making PDO SQL request
+				global $pdo;
+				$stmt = $pdo->prepare($this -> sqlr[$request]);
+				$stmt->execute();
+
+				//Temp array
+				$temp = array();
+
+				if($stmt -> rowCount() > 0){ //check if more than 0 record found
+						
+						//retrieve our table contents
+						//fetch() is faster than fetchAll()
+						//http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
+						while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+							array_push($temp, $row);
+						}
+						return $temp;
+				}else{
+					echo 'empty sql answer';
+					return array();
+				}
+
+			}else{
+				echo 'empty request';
+				return array();
+			}
+		}
+			
+		public function getCategories(){
+			$categories  = $this -> makeSQL('categories');
+			return $categories;
+			// foreach ($categories as $cat) {
+			// 	echo var_dump($cat).'<br/>';
+			// }
+		}
+
+		public function getAdverts(){
+			$advert  = $this -> makeSQL('adverts');
+			return $advert;
+		}
+	}
+
 ?>
