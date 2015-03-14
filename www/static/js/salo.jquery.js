25c414759b60d0d8a56364e63250c9697b29b177
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-03-14 19:45:13 +1300

    Updated template. Improved styling, added tabs.

--- www/static/js/salo.jquery.js
+++ www/static/js/salo.jquery.js
@@ -3,24 +3,41 @@
 		var options = $.extend({}, orguments);
 		var todo = this;
 		var models = {
-			item: function(data){
-				return '<div class="large-3 small-6 columns item" data-id="' + data.id + '">' +
+			advert: function(data){
+				return '<div class="large-3 columns item" data-id="' + data.id + '">' +
 							'<img src="http://placehold.it/1000x1000&amp;text=Thumbnail">' +
 							'<div class="panel">' +
-								'<h5>' + data.title + '</h5>' +
-								'<h6 class="subheader">' + data.text + '</h6>' +
+								'<h5>' + data.title  + '</h5>' +
+								'<p>' + data.text + '</p>' +
 							'</div>' +
 						'</div>';
+			}, 
+			category: function(id, name){
+				return {
+					link: '<li class="tab-title" role="presentational" ><a href="#cat-' + id + '" role="tab" tabindex="0"aria-selected="false" controls="cat-' + id + '">' + name + '</a></li>',
+					tab: '  <section role="tabpanel" aria-hidden="false" class="content" id="cat-' + id + '"><div class="large-12 columns items"></div></section>'
+				}
 			}
 		};
 
 		$.getJSON('http://192.168.55.55/adverts.php').done(function(data){
+				
 
+			// console.log(todo);
 			todo.each(function(){
-				for(advert of data){
-					$(this).append(models.item(advert));
+				for(category of data.categories){
+					var catappend = models.category(category.id, category.loc_ru);
+					$(this).append(catappend.tab);
+					$(options.links).append(catappend.link);
 				}
+				for(advert of data.adverts){
+					$('section#cat-' + advert.id + ' > .items', this).append(models.advert(advert));
+				}
+
 			})
+
+		}).fail(function(e){
+			console.log("ERROR: " + e);
 		})
 	}
 })(jQuery)
