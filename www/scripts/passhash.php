2240cd96ec0668d269cd3150a74d28a7f7c3030c
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-03-30 22:03:40 +1300

    Added EXAMPLE new user SQL generator.

--- www/scripts/passhash.php
+++ www/scripts/passhash.php
@@ -1,8 +1,22 @@
 <?php
+	// //Include for DEBUG
+	// error_reporting(E_ALL); 
+	// ini_set( 'display_errors','1');
+	
+	include __DIR__.'/../includes/auth.php';
 
-	$password = "id10t";
-	$salt = bin2hex(openssl_random_pseudo_bytes(22)); 
-	$challange = "$2a$12$";
-	$hash = crypt($password, $challange.$salt);
-	echo $hash;
+	header('Content-Type: text');
+
+	$cryp = new Auth\Crypto();
+
+	$creds = array(
+		'username' => "test",
+		'password' => "id10t",
+		'challenge' => $cryp -> salt()
+	);
+
+	$response = $cryp -> blowfish( $cryp -> H( $creds['password'], $creds['challenge'] ) );
+
+	echo 'Responce(TEST ONLY): '. $cryp ->H( $creds['password'], $creds['challenge'] )."\n";
+	echo "INSERT INTO `salo`.`users` (`id`, `hash`, `username`, `challenge`) VALUES (NULL, '" . $response . "', '" . $creds['username'] . "', '" . $creds['challenge'] . "');";
 ?>
