64728213b0c38c15e23083e67f014294b4ce8133
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-04-01 14:00:00 +1300

    Moved upload to class

--- www/server/upload.php
+++ www/server/upload.php
@@ -1,17 +1,23 @@
 <?php
-	$allowed_types = array("image/gif", "image/jpg", "image/jpeg", "image/png", "image/bmp");
+	namespace Upload;
+	class Upload{
+		private $allowed_types = array("image/gif", "image/jpg", "image/jpeg", "image/png", "image/bmp");
+		public function upload($value='')
+		{
+			if(is_uploaded_file($_FILES["filename"]["tmp_name"])){
 
-	if(is_uploaded_file($_FILES["filename"]["tmp_name"])){
+				if(in_array($_FILES['filename']['type'], $allowed_types)){ //need to check, maybe i missed some formats
+					$_FILES["filename"]["name"] = md5($_FILES["filename"]["name"].time());
+					move_uploaded_file($_FILES["filename"]["tmp_name"], ".\\uploads".$_FILES["filename"]["name"].'.jpg');
+				}else{
+					echo("Sorry but we need image");
+				}
 
-		if(in_array($_FILES['filename']['type'], $allowed_types)){ //need to check, maybe i missed some formats
-
-			$_FILES["filename"]["name"] = md5($_FILES["filename"]["name"].time());
-			move_uploaded_file($_FILES["filename"]["tmp_name"], ".\\uploads".$_FILES["filename"]["name"].'.jpg');
-	
-		}else{
-			echo("Sorry but we need image");
+			}else{
+				echo("Error");
+			}
 		}
-	}else{
-		echo("Error");
+		
 	}
+	
 ?>
