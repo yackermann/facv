1a437adbacb3dba614801c95366067149a69a883
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-04-02 22:21:27 +1300

    Fixed missed  in upload.

--- www/server/includes/upload.php
+++ www/server/includes/upload.php
@@ -6,7 +6,7 @@
 		{
 			if(is_uploaded_file($_FILES["filename"]["tmp_name"])){
 
-				if(in_array($_FILES['filename']['type'], $allowed_types)){ //need to check, maybe i missed some formats
+				if(in_array($_FILES['filename']['type'], $this -> allowed_types)){ //need to check, maybe i missed some formats
 					$_FILES["filename"]["name"] = md5($_FILES["filename"]["name"].time());
 					move_uploaded_file($_FILES["filename"]["tmp_name"], ".\\uploads".$_FILES["filename"]["name"].'.jpg');
 				}else{
