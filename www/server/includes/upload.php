fd3a76973c723f4584be489c55e901705d94f3e9
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-04-06 16:36:09 +1200

    Finished base64 upload script.

--- www/server/includes/upload.php
+++ www/server/includes/upload.php
@@ -1,23 +1,50 @@
 <?php
-	namespace Upload;
-	class Upload{
-		private $allowed_types = array("image/gif", "image/jpg", "image/jpeg", "image/png", "image/bmp");
-		public function upload($value='')
-		{
-			if(is_uploaded_file($_FILES["filename"]["tmp_name"])){
+    namespace Upload;
+    class Upload{
+        private function getImageMimeType($imagedata){
 
-				if(in_array($_FILES['filename']['type'], $this -> allowed_types)){ //need to check, maybe i missed some formats
-					$_FILES["filename"]["name"] = md5($_FILES["filename"]["name"].time());
-					move_uploaded_file($_FILES["filename"]["tmp_name"], ".\\uploads".$_FILES["filename"]["name"].'.jpg');
-				}else{
-					echo("Sorry but we need image");
-				}
+            function getBytesFromHexString($hexdata){
+                for($count = 0; $count < strlen($hexdata); $count+=2)
+                    $bytes[] = chr(hexdec(substr($hexdata, $count, 2)));
 
-			}else{
-				echo("Error");
-			}
-		}
-		
-	}
-	
+                return implode($bytes);
+            }
+
+            $imagemimetypes = array( 
+                "jpg" => "FFD8", 
+                "png" => "89504E470D0A1A0A", 
+                "gif" => "474946",
+                "bmp" => "424D", 
+                "tiff" => "4949",
+                "tiff" => "4D4D"
+            );
+
+            foreach ($imagemimetypes as $mime => $hexbytes){
+                $bytes = getBytesFromHexString($hexbytes);
+                if (substr($imagedata, 0, strlen($bytes)) == $bytes)
+                    return $mime;
+            }
+            return NULL;
+        }
+
+        private function base64_to_img($base64_string) {
+            $data = base64_decode(explode(',', $base64_string)[1]);
+            $ext = $this -> getImageMimeType($data);
+            if($ext){
+                $filename = md5($base64_string.time()).'.'.$ext;
+                $output_file = $_SERVER['DOCUMENT_ROOT'].'/server/uploads/'.$filename;
+
+                $ifp = fopen( $output_file, "wb" ); 
+                fwrite( $ifp, $data ); 
+                fclose( $ifp ); 
+                return $filename; 
+            }
+            return NULL;
+        }
+        
+        public function upload($image){
+            return $this -> base64_to_img($image);
+        }
+    }
+    
 ?>
