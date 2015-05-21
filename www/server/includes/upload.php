3f4b1549a293e96f356fa8902385e3a5352bcdd8
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-05-21 21:03:18 +1200

    Reverted upload.php.

--- www/server/includes/upload.php
+++ www/server/includes/upload.php
@@ -2,14 +2,11 @@
     namespace Upload;
     class Upload{
         private function getImageMimeType($imagedata){
-
             function getBytesFromHexString($hexdata){
                 for($count = 0; $count < strlen($hexdata); $count+=2)
                     $bytes[] = chr(hexdec(substr($hexdata, $count, 2)));
-
                 return implode($bytes);
             }
-
             $imagemimetypes = array( 
                 "jpg" => "FFD8", 
                 "png" => "89504E470D0A1A0A", 
@@ -18,7 +15,6 @@
                 "tiff" => "4949",
                 "tiff" => "4D4D"
             );
-
             foreach ($imagemimetypes as $mime => $hexbytes){
                 $bytes = getBytesFromHexString($hexbytes);
                 if (substr($imagedata, 0, strlen($bytes)) == $bytes)
@@ -26,25 +22,25 @@
             }
             return NULL;
         }
-
         private function base64_to_img($base64_string) {
-            if(preg_match('/^([A-Za-z0-9+\/]{4})*([A-Za-z0-9+\/]{4}|[A-Za-z0-9+\/]{3}=|[A-Za-z0-9+\/]{2}==)$/i', $base64_string)){
-                $data = base64_decode(explode(',', $base64_string)[1]);
-                $ext = $this -> getImageMimeType($data);
-                if($ext){
-                    try {
-                        $filename = md5($base64_string.time()).'.'.$ext;
-                        $output_file = $_SERVER['DOCUMENT_ROOT'].'/server/uploads/'.$filename;
+            $data = base64_decode(explode(',', $base64_string)[1]);
+            $ext = $this -> getImageMimeType($data);
+            if($ext){
+                try {
 
-                        $ifp = fopen( $output_file, "wb" ); 
-                        fwrite( $ifp, $data ); 
-                        fclose( $ifp ); 
-                        return $filename; 
-                    } catch (Exception $e) {
-                        return NULL;
-                    }
+                    $filename = md5($base64_string.time()).'.'.$ext;
+
+                    // echo $filename;
+                    $output_file = $_SERVER['DOCUMENT_ROOT'].'/server/uploads/'.$filename;
+                    // echo $output_file;
+                    $ifp = fopen( $output_file, 'wb' ); 
+                    fwrite( $ifp, $data ); 
+                    fclose( $ifp ); 
+
+                    return $filename; 
+                } catch (Exception $e) {
+                    return NULL;
                 }
-                return NULL;
             }
             return NULL;
         }
