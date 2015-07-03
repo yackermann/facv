c1d7615eb42629766d2013eace845c938555c0a3
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-07-03 23:42:48 +1200

    Replaced elseifs with switch

--- www/server/user.php
+++ www/server/user.php
@@ -5,85 +5,112 @@
     header('Content-Type: application/json');
 
     if( isset( $_SESSION['logged'] ) && $_SESSION['logged']){
+
         if($_POST && isset($_POST['method'])){
-            if( $_POST['method'] === 'exist' ){
 
-                if( isset($_POST['username']) ){
+            switch ($_POST['method']) {
+                
+                case 'exist':
+
+                    /**
+                     * Check if USER exist
+                     */
+
+                    if( isset($_POST['username']) ){
+                        include __DIR__.'/includes/auth.php';
+                        $e = new \Auth\Exist($_POST['username']);
+                        echo json_encode( array( 'exist' => $e -> exist() ) );
+                    }
+
+                break;
+
+                case 'delete':
+
+                     /**
+                     * Delete user
+                     */
+
+                    include __DIR__.'/includes/sql_requests.php';
+                    $SQLDelete = new SQLRequests\Delete();
+
+                    if( isset($_POST['id']) )
+                        echo json_encode( $SQLDelete -> user() );
+
+                break;
+
+                case 'register':
+
+                    /**
+                     * Register new user
+                     */
+
                     include __DIR__.'/includes/auth.php';
-                    $e = new \Auth\Exist($_POST['username']);
-                    echo json_encode( array( 'exist' => $e -> exist() ) );
-                }
-                    
-            }else if( $_POST['method'] === 'delete' ){
-                include __DIR__.'/includes/sql_requests.php';
-                $SQLDelete = new SQLRequests\Delete();
+                    $reg;
 
-                if( isset($_POST['id']) )
-                    echo json_encode( $SQLDelete -> user() );
+                    if( isset( $_POST['username'] ) ){
+                        //Stage one, get challenge
+                        $reg = new \Auth\Reg($_POST['username']);
 
-            }else if( $_POST['method'] === 'register' ){
+                        if( $reg -> exist() ){
+                            echo json_encode( array('status' => 409, 'errorMessage' => 'User exists.') );
+                        }else{
 
-                include __DIR__.'/includes/auth.php';
-                $reg;
+                            echo json_encode( array( 'challenge' => $reg -> challenge() ) );
+                            $_SESSION['reg'] = serialize( $reg );
+                        }
+                        
 
-                if( isset( $_POST['username'] ) ){
-                    //Stage one, get challenge
-                    //
-                    $reg = new \Auth\Reg($_POST['username']);
+                    }else if( isset($_SESSION['reg']) && isset($_POST['response']) ){ 
+                        
+                        //Stage two verify the response
 
-                    if( $reg -> exist() ){
-                        echo json_encode( array('status' => 409, 'errorMessage' => 'User exists.') );
-                    }else{
+                        $reg = unserialize($_SESSION['reg']);
 
-                        echo json_encode( array( 'challenge' => $reg -> challenge() ) );
-                        $_SESSION['reg'] = serialize( $reg );
+                        $reg -> hash($_POST['response']);
+                        echo json_encode( $reg -> register() );
+                        
+                        unset($_SESSION['reg']);
+                        
                     }
                     
+                break;
 
-                }else if( isset($_SESSION['reg']) && isset($_POST['response']) ){ 
-                    
-                    //Stage two verify the response
+                case 'update':
 
-                    $reg = unserialize($_SESSION['reg']);
+                     /**
+                     * Change password
+                     */
 
-                    $reg -> hash($_POST['response']);
-                    echo json_encode( $reg -> register() );
-                    
-                    unset($_SESSION['reg']);
-                    
-                }
-                
-            }else if( $_POST['method'] === 'update' ){
+                    include __DIR__.'/includes/auth.php';
+                    $update;
 
-                include __DIR__.'/includes/auth.php';
-                $update;
+                    if( isset( $_POST['username'] ) ){
+                        //Stage one, get challenge
+                        $update = new \Auth\Update($_POST['username']);
 
-                if( isset( $_POST['username'] ) ){
-                    //Stage one, get challenge
-                    $update = new \Auth\Update($_POST['username']);
+                        if( $update -> exist() ){
+                            echo json_encode( array( 'challenge' => $update -> challenge() ) );
+                            $_SESSION['update'] = serialize( $update );
+                        }else{
+                            echo json_encode( array('status' => 400, 'errorMessage' => 'User NOT exists.') );
+                            unset($_SESSION['update']);
+                        }
+                        
 
-                    if( $update -> exist() ){
-                        echo json_encode( array( 'challenge' => $update -> challenge() ) );
-                        $_SESSION['update'] = serialize( $update );
-                    }else{
-                        echo json_encode( array('status' => 400, 'errorMessage' => 'User NOT exists.') );
+                    }else if( isset($_SESSION['update']) && isset($_POST['response']) ){ 
+                        
+                        //Stage two verify the response
+
+                        $update = unserialize($_SESSION['update']);
+
+                        $update -> hash($_POST['response']);
+                        echo json_encode( $update -> update() );
+                        
                         unset($_SESSION['update']);
+                        
                     }
-                    
 
-                }else if( isset($_SESSION['update']) && isset($_POST['response']) ){ 
-                    
-                    //Stage two verify the response
-
-                    $update = unserialize($_SESSION['update']);
-
-                    $update -> hash($_POST['response']);
-                    echo json_encode( $update -> update() );
-                    
-                    unset($_SESSION['update']);
-                    
-                }
-                
+                break;
             }
 
         }
